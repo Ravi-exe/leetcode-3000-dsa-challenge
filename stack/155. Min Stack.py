a7ebe0e9c6009c5dd588141eec4dbef908fdb497
ravi-finoux <ravi.finoux@360.one>
2025-12-10 13:23:46 +0530

    Refine MinStack implementation: update push and getMin methods, enhance docstring for clarity.

--- stack/155. Min Stack.py
+++ stack/155. Min Stack.py
@@ -5,24 +5,40 @@ from typing import List
 import math
 
 class MinStack:
+    """
+
+        // embeding option study case
+        stack  3, 1, 5,     -9
+        
+        min    3, 1, 1,     -9
+        actual 0, -2, 4,    -10
+
+        
+        actual formula => val - min
+
+        if actual negative means min change 
+        if positive min not change 
+
+
+
+    """
 
     def __init__(self):
         self._data: List[int] = []
-        self._min = None
+        self._min : List[int] = []
 
-    def push(self, val: int) -> None: 
-        self._min = self._min if self._min != None and self._min < val else val
-        return self._data.append(val)
-        
+    def push(self, val: int) -> None:
+        self._data.append(val)
+        self._min.append(self._min[-1] if len(self._min) > 0 and self._min[-1] < val else val)
 
-    def pop(self) -> None: 
+    def pop(self) -> None:
         self._data.pop()
+        self._min.pop()
         
-
     def top(self) -> int: return self._data[-1]
         
 
-    def getMin(self) -> int: return self._min
+    def getMin(self) -> int: return self._min[-1]
         
 
 
