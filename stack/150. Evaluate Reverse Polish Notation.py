930c20f267b76d6f3c12f28117b27cd5fe9b755c
ravi-finoux <ravi.finoux@360.one>
2025-12-10 16:53:35 +0530

    Add Evaluate Reverse Polish Notation solution skeleton;

---
+++ stack/150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        """
+            example: 5 4 + 8 * 3 -
+                op = ((5 + 4) * 8) - 3)
+                
+                "5 + 4 * 8 - 3"
+
+        """
+        return 0
+
