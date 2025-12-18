a08cba7f7b8d5b5dc2c5fd9f77acd9522bc2ba14
Ravi-exe <Ravi.software@outlook.com>
2025-12-18 12:33:04 +0530

    Refactor Top K Frequent Elements solution: enhance logic for frequency ranking and update docstring

--- hashmap/347. Top K Frequent Elements.py
+++ hashmap/347. Top K Frequent Elements.py
@@ -14,21 +14,47 @@ class Solution:
             rank manipulte
 
             score{
-                1: (4, 1 (1st ind of rank))
-                2: (3, 2 (1st ind of rank))
-                3: (2, 3 (1st ind of rank))
-                4: (1, 4 (1st ind of rank))
+                1: rank ind 0
+                2: rank ind 1
+                3: rank ind 2
+                4: rank ind 3
             }
-            rank [1th,2th,3th,4th]
+            rank [(1,10),(2,9),(3,8),(4,7)]
                    1,  2, 3,  4,
 
 
 
         """
-        rank = []
         map = {}
+        rank = []
         for i in nums:
-            map[i] = map.get(i,0) + 1
-            # if map[i
-            
-        return [rank[i] for i in range(k)]
+            print(i, map, rank)
+            rank_ind = map.get(i)
+            if rank_ind != None:
+                rank[rank_ind] = (rank[rank_ind][0], rank[rank_ind][1] + 1)
+                while 0 <= rank_ind - 1 and rank[rank_ind - 1][1] < rank[rank_ind][1]:
+                        # rank[rank_ind - 1], rank[rank_ind] = rank[rank_ind], rank[rank_ind -1]
+                        # map[rank[rank_ind-1][0]] = rank_ind
+                        # map[rank[rank_ind][0]]
+                        # rank_ind -=1
+
+                        prevRank = rank[rank_ind - 1]
+                        currentRank = rank[rank_ind]
+                        
+                        map[rank[rank_ind][0]] = rank_ind - 1
+                        map[rank[rank_ind - 1][0]] = rank_ind
+                        rank[rank_ind - 1] = currentRank
+                        rank[rank_ind] = prevRank
+
+                        rank_ind -=1
+                        
+            else:
+                rank.append((i,1))
+                map[i] = len(rank) - 1
+        return [rank[i][0] for i in range(k)]
+    
+
+
+soln = Solution()
+
+print(soln.topKFrequent([1,2,2,11,1,1,1,10],2))
