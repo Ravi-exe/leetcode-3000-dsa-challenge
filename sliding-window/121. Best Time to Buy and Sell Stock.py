29b19bd2a671a5aa4cbbb4f75124a5e1dd79b99e
Ravi-exe <ravi.software@outlook.com>
2025-12-30 00:32:01 +0530

    Implement initial structure for maxProfit function in Best Time to Buy and Sell Stock problem

---
+++ sliding-window/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,15 @@
+
+
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+            Input: prices = [7,1,5,3,6,4]
+            Output: 5
+            
+            
+        """
+        pass
