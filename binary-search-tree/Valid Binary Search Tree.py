ad5fb860df17b53cebd9d9d1fa16d51ff52be1a2
Ravi-exe <ravi.software@outlook.com>
2026-01-20 10:33:58 +0530

    Implement solutions for Valid Binary Search Tree, Invert Binary Tree, and Subtree of Another Tree problems; refactor Same Tree, Binary Tree Level Order Traversal, and Maximum Depth of Binary Tree solutions

---
+++ binary-search-tree/Valid Binary Search Tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        """
+                10
+            9          11
+         8    10     7    13
+         
+        """
+
+        
+        
+
+
+
+
+
+
